5ee0484ef4c01bca1f64c36f9df848fc484aba87
Robby <robby.pambudi10@gmail.com>
2024-04-26 22:51:14 +0700

    feat: setup with auth

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError } from 'axios';
 import { UninterceptedApiError } from '@/types/api';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'http://127.0.0.1:8000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
