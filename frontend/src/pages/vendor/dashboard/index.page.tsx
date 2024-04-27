ab798b149ad9620e6224c6436afbd14638695c11
Robby <robby.pambudi10@gmail.com>
2024-04-27 23:26:10 +0700

    feat: adjust landing page

--- frontend/src/pages/vendor/dashboard/index.page.tsx
+++ frontend/src/pages/vendor/dashboard/index.page.tsx
@@ -1,6 +1,12 @@
+
+
+import { formatLocaleDate } from "@/lib/date";
+
+import Alert from "@/components/alert/Alert";
 import DashboardVendorLayout from "@/components/layout/vendor/DashboardVendorLayout";
 import DistributionMap from "@/components/map/DistributionMap";
 import Seo from "@/components/Seo";
+import Tag from "@/components/tag/Tag";
 import Typography from "@/components/typography/Typography";
 
 import dummyMap from "@/constant/map";
@@ -16,12 +22,26 @@ export default function DashboardVendorPage() {
           <Typography as='h1' variant='h1'>
             Rekapitulasi Pengunjung
           </Typography>
+          <div className="mt-4">
+            <Alert variant="secondary">
+              Data akan diperbarui setiap 1 jam sekali oleh sistem secara otomatis.
+            </Alert>
+            <div className="flex items-center justify-start mt-4">
+              <Typography>
+                Pembaruan Terakhir :
+              </Typography>
+              <Tag className='ml-2' color='primary'>
+                {formatLocaleDate(new Date(), 'FULL_DATE_WITH_DAY')}
+              </Tag>
+            </div>
 
-          <DistributionMap initialCenter={
-            [-7.29610874, 112.73666760]
-          }
-            visitor={dummyMap}
-          />
+            <DistributionMap initialCenter={
+              [-7.29610874, 112.73666760]
+            }
+              visitor={dummyMap}
+              className="mt-4"
+            />
+          </div>
         </section>
       </main>
     </DashboardVendorLayout>
