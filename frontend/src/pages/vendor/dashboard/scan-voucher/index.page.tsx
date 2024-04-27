9b69244f3d581cce066074deeae5fb508872d387
daudhiyaa <daud.dhiya31@gmail.com>
2024-04-28 03:21:24 +0700

    fix: scan qr page

--- frontend/src/pages/vendor/dashboard/scan-voucher/index.page.tsx
+++ frontend/src/pages/vendor/dashboard/scan-voucher/index.page.tsx
@@ -4,18 +4,45 @@ import NextImage from '@/components/NextImage';
 import Seo from '@/components/Seo';
 import Typography from '@/components/typography/Typography';
 
-export default withVendorAuth(ScanVoucherPage, 'all');
-const HistoryCard = () => {
+const historyVouchers = [
+  {
+    name: 'Benedion',
+    benefit: 'Diskon 10% KBS Night Zoo',
+    acceptedAt: 'Accepted at 11.44, 27 Apr 2024',
+  },
+  {
+    name: 'Benedion 1',
+    benefit: 'Diskon 10% Feeding Jerapah',
+    acceptedAt: 'Accepted at 11.44, 27 Apr 2024',
+  },
+  {
+    name: 'Benedion 2',
+    benefit: 'Diskon 5% KBS Night Zoo',
+    acceptedAt: 'Accepted at 11.44, 27 Apr 2024',
+  },
+];
+
+export default withVendorAuth(ScanVoucherPage, 'optional');
+
+const HistoryCard = ({
+  nama,
+  benefit,
+  acceptedAt,
+}: {
+  nama: string;
+  benefit: string;
+  acceptedAt: string;
+}) => {
   return (
     <div className=''>
       <Typography as='h4' variant='h4' className=''>
-        Nama
+        {nama}
       </Typography>
       <Typography as='p' className=' text-secondary-500'>
-        Benefit Voucher (Diskon berapa)
+        {benefit}
       </Typography>
       <Typography as='p' className='text-typo-tertiary mt-2'>
-        Accepted at
+        {acceptedAt}
       </Typography>
     </div>
   );
@@ -34,31 +61,33 @@ function ScanVoucherPage() {
           <div className='w-2/3 flex flex-col'>
             <div className='bg-red w-full h-full'>
               <NextImage
-                src='/images/campaign/dumy-detail.png'
+                src='/images/scan-qr.png'
                 alt='Hero Image'
-                width={300}
-                height={700}
+                width={500}
+                height={1000}
               />
             </div>
           </div>
 
-          <div className='w-1 h-full bg-red-900' />
+          <div className='w-10 h-full bg-red-900' />
 
           {/* SECTION KANAN */}
           <div className='w-1/2'>
-            <HistoryCard />
-            <div className='w-3/4 my-4 h-[0.5px] bg-gray-400' />
-            <HistoryCard />
-            <div className='w-3/4 my-4 h-[0.5px] bg-gray-400' />
-            <HistoryCard />
-            <div className='w-3/4 my-4 h-[0.5px] bg-gray-400' />
-            <HistoryCard />
-            <div className='w-3/4 my-4 h-[0.5px] bg-gray-400' />
-            <HistoryCard />
+            {historyVouchers.map((voucher, index) => (
+              <div key={index}>
+                <HistoryCard
+                  nama={voucher.name}
+                  benefit={voucher.benefit}
+                  acceptedAt={voucher.acceptedAt}
+                />
+                {index !== historyVouchers.length - 1 && (
+                  <div className='w-3/4 my-4 h-[0.5px] bg-gray-400' />
+                )}
+              </div>
+            ))}
           </div>
         </section>
       </main>
     </DashboardVendorLayout>
   );
 }
-
