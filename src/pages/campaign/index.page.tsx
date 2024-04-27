403bbb59aa42cd1efb60edb99d0da5c833564ea4
daudhiyaa <daud.dhiya31@gmail.com>
2024-04-27 14:35:53 +0700

    fix: gap card in campaign & event page

--- src/pages/campaign/index.page.tsx
+++ src/pages/campaign/index.page.tsx
@@ -1,73 +1,79 @@
-import { ChevronLeft } from 'lucide-react';
-
-import UnderlineLink from '@/components/links/UnderlineLink';
+import Breadcrumb from '@/components/Breadcrumb';
+import Header from '@/components/layout/Header';
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
 import Typography from '@/components/typography/Typography';
 
 import CampaignCard from '@/pages/section/components/card/campaign-card';
 
 const CampaignPage = () => {
   return (
-    <div className='bg-[#F0F2F5]'>
-      <div className='layout py-20'>
-        <div className='flex flex-col items-start'>
-          <Typography as='h1' className='text-4xl font-semibold font-averta'>
-            Campaign
-          </Typography>
-          <Typography as='p' className='text-typo-secondary mt-4'>
-            Campaign penghijauan yang sedang berlangsung
-          </Typography>
-          <UnderlineLink
-            href='/'
-            className='text-primary-400 mt-4 border-primary-400'
-          >
-            <ChevronLeft /> Back to main page
-          </UnderlineLink>
-        </div>
+    <Layout>
+      <Seo templateTitle='Browse Campaign' />
 
-        <div className='mt-12 flex flex-wrap justify-between gap-5'>
-          <CampaignCard
-            image='/images/dummy-card.png'
-            name='Campaign Title'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
-          />
-          <CampaignCard
-            image='/images/dummy-card.png'
-            name='Campaign Title'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
-          />
-          <CampaignCard
-            image='/images/dummy-card.png'
-            name='Campaign Title'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
-          />
-          <CampaignCard
-            image='/images/dummy-card.png'
-            name='Campaign Title'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
-          />
-          <CampaignCard
-            image='/images/dummy-card.png'
-            name='Campaign Title'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
-          />
-          <CampaignCard
-            image='/images/dummy-card.png'
-            name='Campaign Title'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
-          />
-          <CampaignCard
-            image='/images/dummy-card.png'
-            name='Campaign Title'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
-          />
-          <CampaignCard
-            image='/images/dummy-card.png'
-            name='Campaign Title'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
-          />
+      <main>
+        <Header />
+        <div className='bg-[#F0F2F5]'>
+          <div className='layout pb-20 pt-10'>
+            <div className='flex flex-col items-start'>
+              <Breadcrumb crumbs={['/', '/campaign']} className='mb-4' />
+              <Typography
+                as='h1'
+                className='text-4xl font-semibold font-averta'
+              >
+                Campaign
+              </Typography>
+              <Typography as='p' className='text-typo-secondary mt-4'>
+                Campaign penghijauan yang sedang berlangsung
+              </Typography>
+            </div>
+
+            <div className='mt-10 flex flex-wrap justify-between gap-5 gap-y-16'>
+              <CampaignCard
+                image='/images/dummy-card.png'
+                name='Campaign Title'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
+              />
+              <CampaignCard
+                image='/images/dummy-card.png'
+                name='Campaign Title'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
+              />
+              <CampaignCard
+                image='/images/dummy-card.png'
+                name='Campaign Title'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
+              />
+              <CampaignCard
+                image='/images/dummy-card.png'
+                name='Campaign Title'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
+              />
+              <CampaignCard
+                image='/images/dummy-card.png'
+                name='Campaign Title'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
+              />
+              <CampaignCard
+                image='/images/dummy-card.png'
+                name='Campaign Title'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
+              />
+              <CampaignCard
+                image='/images/dummy-card.png'
+                name='Campaign Title'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
+              />
+              <CampaignCard
+                image='/images/dummy-card.png'
+                name='Campaign Title'
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestias odit ipsa consequuntur similique, minima pariatur est beatae esse aspernatur quia accusamus facilis. Ut minima aut ratione, voluptates recusandae iste.'
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </main>
+    </Layout>
   );
 };
 
--- src/pages/event/index.page.tsx
+++ src/pages/event/index.page.tsx
@@ -1,90 +1,91 @@
-import { ChevronLeft } from 'lucide-react';
-
-import UnderlineLink from '@/components/links/UnderlineLink';
+import Breadcrumb from '@/components/Breadcrumb';
+import Header from '@/components/layout/Header';
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
 import Typography from '@/components/typography/Typography';
 
 import EventCard from '@/pages/section/components/card/event-card';
 
 const EventPage = () => {
   return (
-    <div className='bg-[#F0F2F5]'>
-      <div className='layout py-20'>
-        <div className='flex flex-col items-start'>
-          <Typography as='h1' className='text-4xl font-semibold font-averta'>
-            Event
-          </Typography>
-          <Typography as='p' className='text-typo-secondary mt-4'>
-            Penghijauan mendorong kesadaran dan aksi melalui kegiatan dan
-            edukasi lingkungan.
-          </Typography>
-          <UnderlineLink
-            href='/'
-            className='text-primary-400 mt-4 border-primary-400'
-          >
-            <ChevronLeft /> Back to main page
-          </UnderlineLink>
-        </div>
+    <Layout>
+      <Seo templateTitle='Browse Event' />
 
-        <div className='mt-12 flex flex-wrap justify-between gap-5'>
-          <EventCard
-            deadline='28 Apr 2024'
-            poin={100}
-            count_person='20/100'
-            name='Gowes to Kebun Binatang Surabaya'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
-            image='/images/dummy-card.png'
-          />
-          <EventCard
-            deadline='28 Apr 2024'
-            poin={100}
-            count_person='20/100'
-            name='Gowes to Kebun Binatang Surabaya'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
-            image='/images/dummy-card.png'
-          />
-          <EventCard
-            deadline='28 Apr 2024'
-            poin={100}
-            count_person='20/100'
-            name='Gowes to Kebun Binatang Surabaya'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
-            image='/images/dummy-card.png'
-          />
-          <EventCard
-            deadline='28 Apr 2024'
-            poin={100}
-            count_person='20/100'
-            name='Gowes to Kebun Binatang Surabaya'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
-            image='/images/dummy-card.png'
-          />
-          <EventCard
-            deadline='28 Apr 2024'
-            poin={100}
-            count_person='20/100'
-            name='Gowes to Kebun Binatang Surabaya'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
-            image='/images/dummy-card.png'
-          />
-          <EventCard
-            deadline='28 Apr 2024'
-            poin={100}
-            count_person='20/100'
-            name='Gowes to Kebun Binatang Surabaya'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
-            image='/images/dummy-card.png'
-          />
-          <EventCard
-            deadline='28 Apr 2024'
-            poin={100}
-            count_person='20/100'
-            name='Gowes to Kebun Binatang Surabaya'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
-            image='/images/dummy-card.png'
-          />
+      <main>
+        <Header />
+        <div className='layout pb-20 pt-10'>
+          <div className='flex flex-col items-start'>
+            <Breadcrumb crumbs={['/', '/event']} className='mb-4' />
+            <Typography as='h1' className='text-4xl font-semibold font-averta'>
+              Event
+            </Typography>
+            <Typography as='p' className='text-typo-secondary mt-4'>
+              Penghijauan mendorong kesadaran dan aksi melalui kegiatan dan
+              edukasi lingkungan.
+            </Typography>
+          </div>
+
+          <div className='mt-10 flex flex-wrap justify-between gap-10 gap-y-16'>
+            <EventCard
+              deadline='28 Apr 2024'
+              poin={100}
+              count_person='20/100'
+              name='Gowes to Kebun Binatang Surabaya'
+              description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
+              image='/images/dummy-card.png'
+            />
+            <EventCard
+              deadline='28 Apr 2024'
+              poin={100}
+              count_person='20/100'
+              name='Gowes to Kebun Binatang Surabaya'
+              description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
+              image='/images/dummy-card.png'
+            />
+            <EventCard
+              deadline='28 Apr 2024'
+              poin={100}
+              count_person='20/100'
+              name='Gowes to Kebun Binatang Surabaya'
+              description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
+              image='/images/dummy-card.png'
+            />
+            <EventCard
+              deadline='28 Apr 2024'
+              poin={100}
+              count_person='20/100'
+              name='Gowes to Kebun Binatang Surabaya'
+              description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
+              image='/images/dummy-card.png'
+            />
+            <EventCard
+              deadline='28 Apr 2024'
+              poin={100}
+              count_person='20/100'
+              name='Gowes to Kebun Binatang Surabaya'
+              description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
+              image='/images/dummy-card.png'
+            />
+            <EventCard
+              deadline='28 Apr 2024'
+              poin={100}
+              count_person='20/100'
+              name='Gowes to Kebun Binatang Surabaya'
+              description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
+              image='/images/dummy-card.png'
+            />
+            <EventCard
+              deadline='28 Apr 2024'
+              poin={100}
+              count_person='20/100'
+              name='Gowes to Kebun Binatang Surabaya'
+              description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque voluptates sint deleniti at repellendus assumenda atque fuga natus exercitationem temporibus eveniet repudiandae modi culpa obcaecati, quos nesciunt dolorem odit.'
+              image='/images/dummy-card.png'
+            />
+          </div>
         </div>
-      </div>
-    </div>
+      </main>
+    </Layout>
   );
 };
 
